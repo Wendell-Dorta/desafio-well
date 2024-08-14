8f8bb5ee7b0f6f7930f5511c9536a8decb02dcb5
Wendell Dorta <wendelldorta.ws@outlook.com.br>
2024-08-13 23:32:19 -0300

    correção de palavras no marquee no index.php na raiz do projeto

--- index.php
+++ index.php
@@ -16,8 +16,8 @@
     <!-- anuncio reservas -->
     <div class="container-fluid">
         <div class="anuncio-reservas">
-            <marquee style="font-size: 2rem;"><strong>RESERVAS DISPONIVEIS</strong> | Ganhe sobremesas
-                <strong>GRATIS</strong> | <strong>DESCONTO DE 10%</strong> em <strong>TODAS</strong> as bebidas
+            <marquee style="font-size: 2rem;"><strong>RESERVAS DISPONÍVEIS</strong> | Ganhe sobremesas
+                <strong>GRÁTIS</strong> | <strong>DESCONTO DE 10%</strong> em <strong>TODAS</strong> as bebidas
             </marquee>
         </div>
     </div>
